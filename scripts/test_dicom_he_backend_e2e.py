c79e961376f278c50ef9c384a8e3ec2278eeec95
harshil0014 <harshilvora206@gmail.com>
2026-09-24 19:25:00 +0530

    Configure disaster recovery in DICOM E2E

--- scripts/test_dicom_he_backend_e2e.py
+++ scripts/test_dicom_he_backend_e2e.py
@@ -49,6 +49,9 @@ os.environ["MEDICAL_REGISTRY_RUNTIME_DIR"] = str(runtime / "runtime")
 os.environ["MEDICAL_KEY_ROOT"] = str(runtime / "keys")
 os.environ["MEDICAL_MASTER_KEY_HEX"] = os.urandom(32).hex()
 os.environ["MEDICAL_PLAINTEXT_TMPDIR"] = str(plaintext_ram)
+os.environ["MEDICAL_RECOVERY_BACKUP_PATH"] = str(
+    runtime / "external-backup" / "medical-recovery.medrec"
+)
 os.environ["MEDICAL_ETHEREUM_PRIVATE_LOCATORS"] = str(runtime / "private-locators.json")
 
 from fastapi.testclient import TestClient  # noqa: E402
